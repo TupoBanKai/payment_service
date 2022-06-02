0a306d5924da23ebe786746076b4b5e881aced69
TupoBanKai <leagueoflegends.talk@gmail.com>
2022-06-02 19:49:04 +0300

    correction and bringing into normal form

--- app/services/payment_service.rb
+++ app/services/payment_service.rb
@@ -2,46 +2,75 @@ require "faraday"
 require "faraday/net_http"
 Faraday.default_adapter = :net_http
 
-class Payment
+class PaymentService
   def initialize(transaction)
     @transaction = transaction
-    @return_url = "localhost:3000/"
-    @conn = Faraday.new(
+  end
+
+  def send_new_transaction
+    parsed = parse_json(post_transaction)
+    @transaction.update_column(:order_id, parsed.orderId)
+    parsed.formUrl
+  end
+
+  def self.check_status_active_transactions
+    Transaction.active.each do |transaction|
+      client = Client.find_by(id: transaction.client_id)
+      get_status(client, transaction)
+      status_handler(transaction, parse_json(response))
+    end
+  end
+
+  private
+
+  def faraday_connection
+    @conn ||= Faraday.new(
       url: 'https://web.rbsuat.com',
       params: { param: '1' }
       headers: { 'Content-type' => 'application/json' }
       )
   end
 
-  def send_new_transaction
-    response = @conn.post do |req|
+  def get_status(client, transaction)
+    response = faraday_conenction.get do |req|
+      req.url '/ad/rest/getOrderStatus.do'
+      req.params{
+        "userName": "#{ client.username }",
+        "password": "#{ client.password }",
+        "orderId": "#{ transaction.order_id }"
+      }
+    end
+  end
+
+  def post_transaction
+    response = faraday_conenction.post do |req|
       req.url '/ad/rest/register.do',
       req.headers['Content-type'] = 'applications/json'
       req.body = {
-        "userName": "#{currnet_client.username}",
-        "password": "#{currnet_client.password}",
+        "userName": "#{ currnet_client.username }",
+        "password": "#{ currnet_client.password }",
         "orderNumber": "#{@transaction.product_id}",
-        "amount": "#{Product.find_by(id: @transaction.product_id).amount}"
+        "amount": "#{ product_amount }"
         "returnUrl": 'https://localhost:3000'
        }
      end
-    parsed_json = ActiveSupport::JSON.decode(response)
-    @transaction.update_column(:order_id, parsed_json.orderId)
-    url_for_payment = parsed_json.formUrl
   end
 
-  def check_status_active_transacions
-    Transaction.active.each do |transaction|
-      client = Client.find_by(id: @transaction.client_id)
-      response = @conn.get do |req| # перезаписывается,надо решить
-        req.url '/ad/rest/getOrderStatus.do'
-        req.params[
-          "userName": "#{client.username}",
-          "password": "#{client.password}",
-          "orderId": "#{transaction.order_id}"
-        ]
-      end
-      response
+  def product_amount
+    Product.find_by(id: @transaction.product_id).amount
+  end
+
+  def parse_json(response)
+    JSON.parse(response)
+  end
+
+  def status_handler(transaction, response)
+    status = response.orderStatus
+    case status
+    when 2
+      transaction.update_column(:status, "Paid")
+    when 6, 3
+      transaction.update_column(:status, "Failed")
     end
   end
 end
